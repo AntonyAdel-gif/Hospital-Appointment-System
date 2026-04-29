5bef7ea04eaac6e77773d97a77933ca944c16553
AntonyAdel-gif <antonyadel2005@gmail.com>
2026-04-30 01:11:39 +0300

    Update BST.h

--- BST.h
+++ BST.h
@@ -23,18 +23,78 @@ class BST {
    private:
       appointment *root;
 
-      void insert(appointment *& nodeptr, appointment *& newNode){};
-      void cance_app(int num, appointment *& newNode){};
+      void insert(appointment *& nodeptr, appointment *& newNode) {
+         if (nodeptr == NULL) {
+            nodeptr = newNode;
+         }
+         else if (newNode->priority_level < nodeptr->priority_level) {
+            insert(nodeptr->left, newNode);
+         }
+         else {
+            insert(nodeptr->right, newNode);
+         }
+      }
+      void cancel_app(int num, appointment *& nodeptr) {
+         if (nodeptr->priority_level > num) {
+            cancel_app(num, nodeptr->right);
+         }
+         else if (nodeptr->priority_level < num) {
+            cancel_app(num, nodeptr->left);
+         }
+         else {
+            make_deletion(nodeptr);
+         }
+      }
+      void make_deletion(appointment *& nodeptr) {
+
+         appointment *temp_Node = new appointment;
+
+
+         if (nodeptr == nullptr) {
+            cout << "cannot delete empty appointment" << endl;
+         }
+         else if (nodeptr->right == nullptr) {
+            temp_Node = nodeptr;
+            nodeptr = nodeptr->left;
+            temp_Node = nullptr;
+         }
+         else if (nodeptr->left == nullptr) {
+            temp_Node = nodeptr;
+            nodeptr = nodeptr->right;
+            temp_Node = nullptr;
+         }
+         else {
+            temp_Node =nodeptr->right;
+            while (temp_Node->left) {
+               temp_Node = temp_Node->left;
+            }
+            temp_Node->left = nodeptr->left;
+            temp_Node = nodeptr;
+            nodeptr = nodepyt->right;
+            delete temp_Node;
+         }
+      }
+
+
 
    public:
-      void insert_app(int num){};
-      void cancel(int num){};
+      void insert_app(int num) {
+         appointment * newNode = nullptr;
+         newNode = new appointment;
+
+         newNode->priority_level = num;
+         newNode->left = nullptr;
+         newNode->left = nullptr;
+         insert(root, newNode);
+      }
+
+      void cancel(int num) {
+         cancel_app(num, root)
+      }
 
       void pre_order(appointment *r){};
       void post_order(appointment *r){};
       void in_order(appointment *r){};
-
-
 
 };
 
