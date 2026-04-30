0a702c0521d282676e50fe31ab53022736d68043
AntonyAdel-gif <antonyadel2005@gmail.com>
2026-04-30 05:04:47 +0300

    fixing bst logic
    there is still in error for searching
    as it finds only one appointment

--- BST.h
+++ BST.h
@@ -34,6 +34,8 @@ class BST {
             insert(first_appoint->right, new_appoint);
          }
       }
+
+
       void cancel_appoint(int num, appointment *& appoint) {
          if (appoint->priority_level > num) {
             cancel_appoint(num, appoint->right);
@@ -45,9 +47,12 @@ class BST {
             make_deletion(appoint);
          }
       }
+
+
       void make_deletion(appointment *& appoint) {
 
-         appointment *temp_appoint = new appointment;
+         appointment *temp_appoint = nullptr;
+         temp_appoint = new appointment;
 
 
          if (appoint == nullptr) {
@@ -78,21 +83,29 @@ class BST {
 
 
    public:
-      void insert_appoint(int num) {
-         appointment * newNode = nullptr;
-         newNode = new appointment;
+      void insert_appoint(int num , string n, string dep) {
+         appointment * new_appoint = nullptr;
+         new_appoint = new appointment;
 
-         newNode->priority_level = num;
-         newNode->left = nullptr;
-         newNode->left = nullptr;
-         insert(root, newNode);
+         new_appoint->priority_level = num;
+         new_appoint->medical_department = dep;
+         new_appoint->patient_name = n;
+         new_appoint->left = nullptr;
+         new_appoint->left = nullptr;
+         insert(root, new_appoint);
       }
+
+
 
       void cancel_appoint(int num) {
-         cancel_appoint(num, root);
+         while (Searching_appoint(num) != nullptr) {
+            cancel_appoint(num, root);
+         }
       }
 
-      void pre_order(appointment *r)
+
+
+      /*void pre_order(appointment *r)
       {
          if (r == nullptr)
             return nullptr;
@@ -102,8 +115,8 @@ class BST {
             pre_order(r->left);
             pre_order(r->right);
          }
-      };
-      void post_order(appointment *r)
+      };*/
+      /*void post_order(appointment *r)
       {
          if (r ==nullptr)
             return nullptr;
@@ -113,38 +126,75 @@ class BST {
             post_order(r->right);
             cout<<"["<<r->patient_name<<", " <<r->priority_level<<", "<< r->medical_department << "]" <<endl;
          }
-      }
-      void in_order(appointment *r)
-      {
-         if (r == nullptr)
-            return nullptr;
-         else
-         {
-            in_order(r->left);
-            cout<<"["<<r->patient_name<<", " <<r->priority_level<<", "<< r->medical_department << "]" <<endl;
-            in_order(r->left);
+      }*/
 
+
+
+      void display_all(appointment *r){ //in order
+         if (r == nullptr) {
+            return;
+         }
+         else{
+            display_all(r->left);
+            cout<<"["<<r->patient_name<<", " <<r->priority_level<<", "<< r->medical_department << "]" <<endl;
+            display_all(r->right);
+         }
+      }
+
+
+      void display_more_urgent(appointment* r, int num){
+            if (r == nullptr) return;
+
+            display_more_urgent(r->left, num);
+
+            if (r->priority_level <= num)
+               cout << "[" << r->patient_name << ", "
+                    << r->priority_level << ", "
+                    << r->medical_department << "]" << endl;
+
+            display_more_urgent(r->right, num);
          }
 
+
+
+      void display_less_urgent(appointment* r, int num){
+            if (r == nullptr) return;
+
+            display_less_urgent(r->left, num);
+
+            if (r->priority_level >= num)
+               cout << "[" << r->patient_name << ", "
+                    << r->priority_level << ", "
+                    << r->medical_department << "]" << endl;
+
+            display_less_urgent(r->right, num);
       }
-   appointment* Search(appointment* r, int key)
-      {
+
+
+
+      appointment* Search(appointment* r, int key){
          if (r == nullptr)
             return nullptr;
          else if (r->priority_level == key)
             return r;
-         else if (key < r->priority_level)
+         else if (key <= r->priority_level)
             return Search(r->left, key);
          else
             return Search(r->right, key);
       }
-   bool Search(int key)
-      {
+   
+
+      appointment* Searching_appoint(int key){
+
          appointment* result = Search(root, key);
-         if (result==nullptr)
-            return false;
-         else
-            return true;
+         if (result==nullptr) {
+            cout << "Searching appointment does not exist" << endl;
+            return nullptr;
+         }
+         else {
+            return result;
+         }
+
       }
 
 };
